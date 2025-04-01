4c1b45cc345d4a8e80e3c18d88e7c8a9338cf112
Marcos Janchuk <marcosjanchuk@gmail.com>
2025-04-01 17:36:08 -0300

    se agrego la variable a

--- practicaPy.py
+++ practicaPy.py
@@ -1,3 +1,6 @@
 nombre = input("Ingrese un nombre")
 apellido = input("Ingrese su apellido")
 print(f"Su nombre es {nombre} y su apellido {apellido}")
+
+a = 54.67
+print(f"a value is: {a} and it´s an {type(a)}")
